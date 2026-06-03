1552214bbba30bed652e16b661962e3059724cae
Tamir Yamaleyev <tamirka8@gmail.com>
2026-06-03 18:43:05 +0300

    Finished

--- Homework2/DynamicIntArray.cpp
+++ Homework2/DynamicIntArray.cpp
@@ -1,4 +1,5 @@
 #include "DynamicIntArray.h"
+#include <iostream>
 
 DynamicIntArray::DynamicIntArray(size_t n) 
 {
@@ -10,11 +11,28 @@ DynamicIntArray::DynamicIntArray(size_t n)
 	{
 		data[i] = 0;
 	}
+
+	std::cout << "Created an array with size: " << n << std::endl;
 }
 
 DynamicIntArray::~DynamicIntArray() 
 {
+std::cout << "Deleting array" << std::endl;
 	delete[] data;
+}
+
+DynamicIntArray::DynamicIntArray(const DynamicIntArray& other) 
+{
+	std::cout << "Copying array" << std::endl;
+
+	size = other.size;
+	capacity = other.capacity;
+	data = new int[capacity];
+
+	for (size_t i = 0; i < size; i++)
+	{
+		data[i] = other.data[i];
+	}
 }
 
 void DynamicIntArray::Resize(size_t newCapacity)
@@ -31,9 +49,14 @@ void DynamicIntArray::Resize(size_t newCapacity)
 	capacity = newCapacity;
 }
 
-int& DynamicIntArray::Get(int i) 
+const int& DynamicIntArray::Get(int i) const
 {
 	return data[i];
+}
+
+void DynamicIntArray::Set(int i, int v)
+{
+	data[i] = v;
 }
 
 void DynamicIntArray::PushBack(int value)
@@ -46,6 +69,28 @@ void DynamicIntArray::PushBack(int value)
 
 	data[size] = value;
 	size++;
+}
+
+DynamicIntArray& DynamicIntArray::operator=(const DynamicIntArray& other)
+{
+	if (this == &other)
+	{
+		return *this;
+	}
+
+	delete[] data;
+
+	size = other.size;
+	capacity = other.capacity;
+
+	data = new int[capacity];
+
+	for (size_t i = 0; i < size; i++)
+	{
+		data[i] = other.data[i];
+	}
+
+	return *this;
 }
 
 int& DynamicIntArray::operator[](size_t index)
--- Homework2/Homework2.cpp
+++ Homework2/Homework2.cpp
@@ -1,8 +1,86 @@
 #include <iostream>
 #include "DynamicIntArray.h"
 
+void TakeByValue(DynamicIntArray arr)
+{
+    arr.Set(0, 123);
+}
+
 int main()
 {
-    DynamicIntArray arr;
-    std::cout << "Hello World!\n";
+    // Creating an array
+    DynamicIntArray a(5);
+
+    a.Set(0, 7);
+    a.Set(1, 82);
+    a.Set(2, 2);
+
+    std::cout << "A: " << std::endl;
+    for (size_t i = 0; i < a.Size(); i++)
+        std::cout << a.Get(i) << ", ";
+    std::cout << std::endl << std::endl;
+
+    // Copy constructor
+    DynamicIntArray b = a;
+
+    b.Set(1, 200);
+
+    std::cout << "A after B was modified: " << std::endl;
+    for (size_t i = 0; i < a.Size(); i++)
+        std::cout << a[i] << ", ";
+    std::cout << std::endl;
+
+    std::cout << "B: ";
+    for (size_t i = 0; i < b.Size(); i++)
+        std::cout << b[i] << ", ";
+    std::cout << std::endl << std::endl;
+
+    // Copy assignment
+    DynamicIntArray c(5);
+    c = a;
+
+    c.Set(2, 999);
+
+    std::cout << "A after C was modified: " << std::endl;
+    for (size_t i = 0; i < a.Size(); i++)
+        std::cout << a[i] << ", ";
+    std::cout << std::endl;
+
+    std::cout << "C: ";
+    for (size_t i = 0; i < c.Size(); i++)
+        std::cout << c[i] << ", ";
+    std::cout << std::endl << std::endl;
+
+    std::cout << "Self assignment" << std::endl;
+    c = c;
+
+    std::cout << "c after self assignment: " << std::endl;
+    for (size_t i = 0; i < c.Size(); i++)
+        std::cout << c[i] << ", ";
+    std::cout << std::endl << std::endl;
+
+    std::cout << "Pass by value: " << std::endl;
+    TakeByValue(a);
+
+    std::cout << "a after function call: " << std::endl;
+    for (size_t i = 0; i < a.Size(); i++)
+        std::cout << a[i] << " ";
+    std::cout << std::endl << std::endl;
+
+    std::cout << "Pushback and resize: " << std::endl;
+    DynamicIntArray d(2);
+    d.Set(0, 1);
+    d.Set(1, 2);
+
+    d.PushBack(3);
+    d.PushBack(4);
+    d.PushBack(5); // forces resize
+
+    for (size_t i = 0; i < d.Size(); i++)
+        std::cout << d[i] << ", ";
+    std::cout << std::endl;
+
+    std::cout << "Destructors: " << std::endl;
+
+    return 0;
 }
